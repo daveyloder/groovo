546edd7d3c222d33c46ac1148c20a6a21f5c0abf
HonzTM <tup84792@temple.edu>
2025-03-25 15:02:42 -0400

    Added UI card component as a general test

---
+++ src/components/cards/CardComponent.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const CardComponent = () => {
+    return (
+        <>
+        <div>
+            <h1>This is part of the Card Component</h1>
+        </div>
+        </>
+    )
+}
+
+export default CardComponent;
