37e033642ac75020e2049a6b69a3ad701f748ff6
Mike <mxbalogun@gmail.com>
2025-03-30 19:42:49 -0400

    Added modal functionality to navbar

--- src/components/navbar/NavbarComponent.jsx
+++ src/components/navbar/NavbarComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./navbar.module.css";
 
+
 import {
   Collapse,
   Navbar,
@@ -13,9 +14,14 @@ import {
   Input,
   InputGroup,
   Form,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from "reactstrap";
 
 import { FaSearch, FaUser } from "react-icons/fa";
+import LoginComponent from "../login/LoginComponent";
 
 function NavbarComponent() {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,6 +33,9 @@ function NavbarComponent() {
     // Add search logic here later
     console.log("Searching for: ", searchQuery);
   };
+
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => setModal(!modal);
 
   return (
     <Navbar expand="lg" className={`px-3 ${styles.navbar}`} dark>
@@ -59,10 +68,15 @@ function NavbarComponent() {
           </Form>
           <div className={`d-flex ${styles.authLinks}`}>
             <NavItem>
-              <NavLink className={styles.navLink} href="#">
+              <NavLink className={styles.navLink} href="#" onClick={toggleModal}>
                 <FaUser className="me-1" />
                 Login
               </NavLink>
+
+              <Modal isOpen={modal} toggle={toggleModal}>
+                <LoginComponent />
+              </Modal>
+        
             </NavItem>
             <NavItem>
               <NavLink className={styles.navLink} href="/">
