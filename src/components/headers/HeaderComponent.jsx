792f933aacae9b50aae03abb50c128ca4bb92e72
HonzTM <tup84792@temple.edu>
2025-03-31 12:46:46 -0400

    Edited the header/opening page to fit in a little more, aesthetics-wise

--- src/components/headers/HeaderComponent.jsx
+++ src/components/headers/HeaderComponent.jsx
@@ -5,7 +5,8 @@ export default function Header() {
     return(
         <>
     <div className={styles.header}>
-        <h1>Welcome to Groovo! Begin by typing any song or artist in the search bar, and search results will automatically pop up.</h1>
+        <h1>Welcome to Groovo!</h1>
+        <h3>Begin by typing any song or artist in the search bar, and search results will automatically pop up.</h3>
     </div>
         </>
     )
