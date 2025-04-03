173535e1bc3cf00c4e08232a79ecf13fa44ec6e7
Mike <mxbalogun@gmail.com>
2025-04-03 12:22:42 -0400

    Created create accoutn component and stylesheet

---
+++ src/components/createAccount/createAccount.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import styles from "./createAccount.modules.css";
+
+const CreateAccount = () => {
+
+    return (
+        <h1>Hello JAMEs</h1>
+    )
+
+}
+
+export default CreateAccount;
