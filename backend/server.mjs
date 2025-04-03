b74ad82949b4ccb8ad428093b3ca5a12cba92105
daveyloder <davidloder92@gmail.com>
2025-04-03 12:50:15 -0400

    Updated backend to listen for spotify api calls

--- backend/server.mjs
+++ backend/server.mjs
@@ -6,5 +6,47 @@ import fetch from "node-fetch";
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-const baseUrl = process.env.GENIUS_BASE_URL;
+app.post("api/spotify/token", async (req, res) => {
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  if (!clientId || !clientSecret) {
+    return res.status(500).json({ error: "Spotify API keys are missing! " });
+  }
+
+  const authString = Buffer.from(`${clientId}:${clientSecret}`).toString(
+    "base64"
+  );
+
+  try {
+    const response = await fetch("https//acounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${suthString}`,
+      },
+      body: "grant_type=client_credentials",
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      return res
+        .status(400)
+        .json({ error: "Spotify Token Fetch Error", details: errorData });
+    }
+
+    const data = await response.json();
+    res.json({ access_token: data.access_token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on prot ${PORT}`));
