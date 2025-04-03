e0b329604037fadbf48c84f54eb7848302f84ead
daveyloder <davidloder92@gmail.com>
2025-04-03 14:30:59 -0400

    Fixed No Result Ui to be in center of screen

---
+++ frontend/src/components/cards/CardComponent.test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import CardComponent from "./CardComponent";
+import { describe, it, expect } from "vitest";
+import "@testing-library/jest-dom/vitest";
+
+describe("CardComponent");
