0473edb645fce3ccddd7c0ff445d80fb5968a9b1
Mike <mxbalogun@gmail.com>
2025-04-04 16:36:00 -0400

    Created a functionto handle login button

--- frontend/src/components/login/LoginComponent.jsx
+++ frontend/src/components/login/LoginComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Login.module.css";
 import { Form, Label, Input, FormGroup, Button, ModalBody } from "reactstrap";
 
@@ -6,29 +6,31 @@ import { FaUser, FaLock } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 
 const LoginComponent = ({ toggle }) => {
+  const [message, setMessage] = useState("");
+  
+  function handleLogin() {
+    //Grabs username and password from localStorage
+    let username = localStorage.getItem("Username");
+    let password = localStorage.getItem("Password");
+
+    console.log(username);
+    console.log(password);
+    
+  }
+
   return (
     <Form className={styles.formContainer}>
       <h1 className={styles.formHeading}>LOGIN</h1>
       <FormGroup floating>
-        <Input
-          id="username"
-          name="username"
-          placeholder="username"
-          type="text"
-        />
+        <Input id="username" name="username" placeholder="username" type="text"/>
         <Label for="username">Username</Label>
       </FormGroup>{" "}
       <FormGroup floating>
-        <Input
-          id="password"
-          name="password"
-          placeholder="Password"
-          type="password"
-        />
+        <Input id="password" name="password" placeholder="Password" type="password"/>
         <Label for="password">Password</Label>
       </FormGroup>{" "}
-      <Button className={styles.formButton} color="success">
-        Submit
+      <Button className={styles.formButton} onClick={handleLogin} color="success">
+        Login
       </Button>
       <div className={styles.closeButton}>
         <Button onClick={toggle} size="sm">
